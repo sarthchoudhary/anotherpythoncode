1d5290cf9b8c424209001c6593260b954d79e7c1
Sarthak Choudhary <sarthakchoudhary@live.in>
2018-09-25 23:41:09 +0530

    Status:Working Changes: takes care about

--- checks_folders.py
+++ checks_folders.py
@@ -14,14 +14,18 @@ dest_dir = r'C:\Users\Sarth.choudhary\Desktop\destination'
 dir_list = os.walk(source_dir)
 dir_list1 = list(dir_list)
 file_list = []
-temp_folder = os.path.join(source_dir, 'temp')
+#temp_dir = os.path.join(source_dir, 'temp')
+#if not os.path.exists(temp_dir):
+#    os.makedirs(temp_dir)
 for i in range(len(dir_list1)):
     if not dir_list1[i][2] == []:
         a = Path(dir_list1[i][0])
-        b = (dir_list1[i][2])
-        b = ''.join(b)
-        file_path = a / b
-        file_list.append(file_path)
+        dir_list2 = dir_list1[i][2]
+        for k in range(len(dir_list2)):
+            b=dir_list2[k]
+            b = ''.join(b)
+            file_path = a / b
+            file_list.append(file_path)
 
 for j in range(len(file_list)):
     file_name = basename(file_list[j])
@@ -30,9 +34,16 @@ for j in range(len(file_list)):
     if not os.path.isfile(file_path):           #1 no overwrites
         copy2(file_list[j], dest_dir)   #move vs copy
     else:
+#        copy2(file_list[j], temp_dir)        
         [name,ext] = os.path.splitext(file_name)
-        new_file_name = name + '-1' + ext
+        cntr = 1
+        new_file_name = name + '-{}'.format(cntr)  + ext
+        dest_file_name = dest_dir / new_file_name
+        while os.path.isfile(dest_file_name):
+            cntr = cntr + 1
+            new_file_name = name + '-{}'.format(cntr)  + ext
+            dest_file_name = dest_dir / new_file_name
         new_file_name_with_dir = os.path.dirname(file_list[j]) + "\\" + new_file_name
-        copy2()
         os.rename(file_list[j], new_file_name_with_dir)
         copy2(new_file_name_with_dir, dest_dir)
+#os.rmdir(temp_dir)
